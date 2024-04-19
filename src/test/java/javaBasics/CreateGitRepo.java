5840dd7babc2599e7f1a9149e189ef08282248bb
Bhandari <natasha.bhandari@siemens.com>
2024-04-19 17:39:35 +0530

    first commit

---
+++ src/test/java/javaBasics/CreateGitRepo.java
@@ -0,0 +1,22 @@
+package javaBasics;
+
+public class CreateGitRepo {
+    public static void main(String[] args) {
+
+        //Steps to create git reposatory to link code with git hub:
+        // ->   git init
+        // -> git status
+                // if issue came in red color for adding status (then use command git add .)
+                //git status
+        // ->  git add README.md
+        // ->   git commit -m "first commit"
+        // ->   git branch -M main
+        // ->  git remote add origin https://github.com/NatashaBhandari/Day2LearningGitAndJavaSelenium.git
+        // ->   git push -u origin main
+
+        //This command need to run in terminal
+        //how to open terminal -> click on java folder->Open in-> Terminal
+
+
+    }
+}
---
+++ src/test/java/javaBasics/SeleniumJava.java
@@ -0,0 +1,40 @@
+package javaBasics;
+
+public class SeleniumJava {
+
+    public static void main(String[] args)
+    {
+         System.out.println("Hello world"); //Print statement
+        //=============================================
+        //data types and concatenation
+        int year=2000;
+        int age=24;
+        int date=year+age;
+        String name="Natasha";
+        System.out.println("age: "+ age); // Ans-> age: 24
+        System.out.println("Addition "+ age+year);// Ans-> Addition 242000
+        System.out.println("date "+ date);// Ans-> date 2024
+        System.out.println( name +" age is:" +year);
+
+        boolean Gender=true;
+        System.out.println("Are you female: " +Gender); //Ans=> Are you female: true
+
+        short computerBytes=12345;
+        System.out.println("computer memeory: "+ computerBytes); // Ans-> computerBytes: 12345
+
+        long AadharNo=12383727479279822l;
+        System.out.println("AadharNo: "+ AadharNo); // Ans-> AadharNo: 12383727479279822
+
+        float weight=55.23f;
+        System.out.println("weight: "+ weight); // Ans-> weight: 55.23
+
+        double apple=333;
+        System.out.println("apple kgs: "+ apple); // Ans-> apple kgs: 333.0
+
+        char character='A';
+        System.out.println("character is " +character);
+
+        //==========================================================================================
+
+    }
+}
